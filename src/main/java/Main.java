e305e4c928cc40eafeea57516de428887eadeec0
ImGabreuw <imgabreuw.dev@gmail.com>
2024-04-03 15:33:31 -0300

    fix: treat InputMismatchException

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,11 +11,12 @@ public class Main {
 
         Parser parser = new Parser();
         Evaluator evaluator = new Evaluator();
-        byte choice = 0;
+        byte choice;
 
         try (Scanner scanner = new Scanner(System.in)) {
-            while (choice != 5) {
-                System.out.println("""
+            while (true) {
+                try {
+                    System.out.println("""
                         1. Entrada da expressão aritmética na notação infixa.
                         2. Entrada dos valores numéricos associados às variáveis.
                         3. Conversão da expressão, da notação infixa para a notação posfixa, e exibição da expressão
@@ -25,80 +26,84 @@ public class Main {
                         5. Encerramento do programa.
                         """);
 
-                System.out.print("Digite uma das opções apresentadas: ");
-                choice = scanner.nextByte();
-                scanner.nextLine(); // Consumir a quebra de linha
+                    System.out.print("Digite uma das opções apresentadas: ");
+                    choice = scanner.nextByte();
+                    scanner.nextLine(); // Consumir a quebra de linha
 
-                switch (choice) {
-                    case 1:
-                        System.out.print("Digite uma expressão aritmética de notação infixa: ");
-                        arithmeticExpression = scanner.nextLine();
-                        break;
-
-                    case 2:
-                        if (arithmeticExpression.isEmpty()) {
-                            System.out.println("Não foi digitada uma expressão de notação infixa!\nVoltando para o menu...");
+                    switch (choice) {
+                        case 1:
+                            System.out.print("Digite uma expressão aritmética de notação infixa: ");
+                            arithmeticExpression = scanner.nextLine().trim();
                             break;
-                        }
 
-                        for (char c : arithmeticExpression.toCharArray()) {
-                            if (!Character.isLetter(c)) continue;
-
-                            System.out.printf("Digite um valor para a variável %c : ", c);
-                            double value = scanner.nextDouble();
-                            values.add(value);
-                        }
-                        break;
-
-                    case 3:
-                        if (arithmeticExpression.isEmpty()) {
-                            System.out.println("Não foi digitada uma expressão de notação infixa!\nVoltando para o menu...");
-                            break;
-                        }
-
-                        postfixExpression = parser.toPostfixNotion(arithmeticExpression);
-                        System.out.println(postfixExpression);
-                        break;
-
-                    case 4:
-                        if (postfixExpression.isEmpty()) {
-                            throw new IllegalArgumentException("Não existe uma expressão de notação posfixa!\nVoltando para o menu...");
-                        }
-
-                        double[] valuesIn = new double[values.size()];
-                        for (int i = 0; i < values.size(); i++) {
-                            valuesIn[i] = values.get(i);
-                        }
-
-                        double result = evaluator.evaluate(postfixExpression, valuesIn);
-
-                        System.out.println("A expressão é :" + postfixExpression);
-
-                        char[] expression = arithmeticExpression.toCharArray();
-                        int count = 0;
-
-                        for (int i = 0; i < expression.length; i++) {
-                            if (Character.isLetter(expression[i])) {
-                                System.out.println("Variável " + expression[i] + " possui valor " + valuesIn[count++]);
+                        case 2:
+                            if (arithmeticExpression.isEmpty()) {
+                                System.out.println("Não foi digitada uma expressão de notação infixa!\nVoltando para o menu...");
+                                break;
                             }
-                        }
 
-                        System.out.println("O resultado da expressão é " + result);
-                        break;
+                            for (char c : arithmeticExpression.toCharArray()) {
+                                if (!Character.isLetter(c)) continue;
 
-                    case 5:
-                        System.out.println("Finalizando o programa....");
-                        return;
+                                System.out.printf("Digite um valor para a variável %c : ", c);
+                                double value = scanner.nextDouble();
+                                values.add(value);
+                            }
+                            break;
 
-                    default:
-                        System.out.println("Você não digitou uma das opções apresentadas.\nDigite novamente uma nova opção");
-                        break;
+                        case 3:
+                            if (arithmeticExpression.isEmpty()) {
+                                System.out.println("Não foi digitada uma expressão de notação infixa!\nVoltando para o menu...");
+                                break;
+                            }
+
+                            postfixExpression = parser.toPostfixNotion(arithmeticExpression);
+                            System.out.println(postfixExpression);
+                            break;
+
+                        case 4:
+                            if (postfixExpression.isEmpty()) {
+                                throw new IllegalArgumentException("Não existe uma expressão de notação posfixa!\nVoltando para o menu...");
+                            }
+
+                            double[] valuesIn = new double[values.size()];
+                            for (int i = 0; i < values.size(); i++) {
+                                valuesIn[i] = values.get(i);
+                            }
+
+                            double result = evaluator.evaluate(postfixExpression, valuesIn);
+
+                            System.out.println("A expressão é :" + postfixExpression);
+
+                            char[] expression = arithmeticExpression.toCharArray();
+                            int count = 0;
+
+                            for (char c : expression) {
+                                if (Character.isLetter(c)) {
+                                    System.out.println("Variável " + c + " possui valor " + valuesIn[count++]);
+                                }
+                            }
+
+                            System.out.println("O resultado da expressão é " + result + ".");
+                            break;
+
+                        case 5:
+                            System.out.println("Finalizando o programa....");
+                            return;
+
+                        default:
+                            System.out.println("Você não digitou uma das opções apresentadas.\nDigite novamente uma nova opção");
+                            break;
+                    }
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
+                } catch (InputMismatchException e) {
+                    System.out.println("Você deve digitar uma opção válida.\nVoltando para o menu....");
+                    scanner.nextLine(); // Consumir a quebra de linha pendente
                 }
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
-        } catch (InputMismatchException e) {
-            throw new IllegalArgumentException("Você deve digitar uma opção válida.\nVoltando para o menu....");
+        } catch (Exception e) {
+            System.out.println("Uma erro inesperado acontenceu.");
         }
     }
 }
